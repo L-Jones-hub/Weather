ea911235520a4b3f7f0b687fa502d76a12b60a47
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-05-18 11:59:40 -0500

    Fixed buttons

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
   iconElement.setAttribute("alt", response.data.weather[0].description);
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
