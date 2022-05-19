3667aa4e124d39f6907075b34a3742f42e1f9a34
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-05-19 11:27:21 -0500

    Formatted forecast days

--- script.js
+++ script.js
@@ -21,30 +21,45 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecast = response.data.daily;
   let forecastHTML = `<div class="row" >`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          <img src="http://openweathermap.org/img/wn/50d@2x.png"
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          
+          <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max">
-              18˚
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}˚
             </span>
             <span class="weather-forecast-temperature-min">
-              12˚
+              ${Math.round(forecastDay.temp.min)}˚
             </span>
             </div>
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -90,8 +105,8 @@ function searchCity(city) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input").value;
-  search(cityInputElement);
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
 function displayFahrenheitTemperature(event) {
